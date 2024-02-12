fef792c30d8ecff42c111ba643991c175fd3e66a
AdenChiu22 <142437577+AdenChiu22@users.noreply.github.com>
2024-02-12 12:52:05 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -35,15 +35,20 @@ public void tester()
     }
   }
 }
-public boolean palindrome(String word)
+public boolean palindrome(String sWord)
 {
-  //your code here
+  String s = "";
+  for(int i = sWord.length()-1; i >= 0; i--)
+  s+= sWord.charAt(i);
+  if(s.equals(sWord))
+  return true;
   return false;
 }
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
+     for(int i = str.length()-1; i >= 0; i--)
+  sNew+= str.substring(i,i+1);
     return sNew;
 }
 }
